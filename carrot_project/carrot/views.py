9434fa6c4d18110e3b7a86f98bb5a6479e999739
minsuhaha <haminsu5@gmail.com>
2023-09-26 09:55:19 +0900

    feat : add <하민수/회원가입 view>

--- carrot_project/carrot/views.py
+++ carrot_project/carrot/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import CustomRegistrationForm
 
-# Create your views here.
+
+# 유저 회원 가입
+def register(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = CustomRegistrationForm(request.POST)
+        username = request.POST.get('username')
+        if User.objects.filter(username=username).exists():
+            error_message = "이미 존재하는 아이디입니다."
+        elif form.is_valid():
+            
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            
+            # 비밀번호 일치 여부를 확인
+            if password1 == password2:
+                # 새로운 유저를 생성 (create_user 함수 이용)
+                user = User.objects.create_user(username=username, password=password1)
+                
+                # 유저를 로그인 상태로 만듦
+                login(request, user)
+            
+                return redirect('login')
+            
+            else:
+                form.add_error('password2', 'Passwords do not match')
+    else:
+        form = CustomRegistrationForm()
+    
+    return render(request, 'registration/register.html', {'form': form, 'error_message': error_message})
