896f9c686e3b86d198acd2db4714130fe4f7ae1a
hoi <coghd28@naver.com>
2023-09-26 13:32:00 +0900

    feat : add <김채홍/urls.py>
    로그인, 로그아웃 url 연결

--- carrot_project/carrot/urls.py
+++ carrot_project/carrot/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from . import views
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     #어플리케이션 연결
+    path('', views.main, name='main'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='main'), name='logout'),
 ]
