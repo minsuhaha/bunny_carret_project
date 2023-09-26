ed4c7237c680094ba01f383431bdde3ab9924c37
minsuhaha <haminsu5@gmail.com>
2023-09-26 16:10:28 +0900

    feat : add <하민수/중고거래상세 뷰>

--- carrot_project/carrot/views.py
+++ carrot_project/carrot/views.py
@@ -1,11 +1,38 @@
-from .models import Product
-from django.shortcuts import render
+from .models import Product, UserProfile
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .forms import CustomLoginForm, CustomRegistrationForm
 
+# 메인 화면
+def main(request):
+  top_views_products = Product.objects.filter(product_sold='N').order_by('-view_cnt')[:4]
+  return render(request, 'carrot_app/main.html', {'products' : top_views_products})
 
+#로그인
+def user_login(request):
+ # 이미 로그인한 경우
+    if request.user.is_authenticated:
+        return redirect('main')
+    
+    else:
+        form = CustomLoginForm(data=request.POST or None)
+        if request.method == 'POST':
+           
+             # 입력정보가 유효한 경우 각 필드 정보 가져옴
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+            
+                user = authenticate(request, username=username, password=password)
+
+            #로그인이 성공한 경우
+            if user is not None:
+                login(request, user)
+                return redirect('main')  # 로그인 성공 후'main'은 리디렉션할 URL의 이름 혹은 경로
+        return render(request, 'registration/login.html', {'form': form})
+    
 # 유저 회원 가입
 def register(request):
     error_message = ''
@@ -41,31 +68,30 @@ def trade(request):
     top_posts = Product.objects.filter(product_sold='N').order_by('-view_cnt') # 아직 팔리지 않은 물품중에 조회수 나열
     return render(request, 'carrot_app/trade.html', {'posts': top_posts})
 
-
-# 메인 화면
-def main(request):
-  top_views_products = Product.objects.filter(product_sold='N').order_by('-view_cnt')[:4]
-  return render(request, 'carrot_app/main.html', {'products' : top_views_products})
-
-#로그인
-def user_login(request):
- # 이미 로그인한 경우
-    if request.user.is_authenticated:
-        return redirect('main')
+# 중고거래 상세정보 화면
+def trade_post(request, pk):
+    post = get_object_or_404(Product, pk=pk)
     
-    else:
-        form = CustomLoginForm(data=request.POST or None)
-        if request.method == 'POST':
-           
-             # 입력정보가 유효한 경우 각 필드 정보 가져옴
-            if form.is_valid():
-                username = form.cleaned_data['username']
-                password = form.cleaned_data['password']
-            
-                user = authenticate(request, username=username, password=password)
+    # 조회수 증가
+    if request.user.is_authenticated: # 요청한 유저가 로그인이 되어있다면
+        if request.user != post.seller: # 해당 게시글을 작성한 유저와 다르다면
+            post.view_cnt += 1 # 조회수 1 증가
+            post.save()
 
-            #로그인이 성공한 경우
-            if user is not None:
-                login(request, user)
-                return redirect('main')  # 로그인 성공 후'main'은 리디렉션할 URL의 이름 혹은 경로
-        return render(request, 'registration/login.html', {'form': form})
+    else:
+        post.view_cnt += 1
+        post.save()
+
+    try:
+        user_profile = UserProfile.objects.get(user=post.seller)
+    except UserProfile.DoesNotExist:
+            user_profile = None
+
+    context = {
+        'post': post,
+        'user_profile': user_profile,
+    }
+
+    return render(request, 'carrot_app/trade_post.html', context)
+
+
