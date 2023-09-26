c105c420dcf247c7139087d70dfe2cb4be16d51b
minsuhaha <haminsu5@gmail.com>
2023-09-26 16:10:10 +0900

    feat : add <하민수/중고거래상세화면 url>

--- carrot_project/carrot/urls.py
+++ carrot_project/carrot/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
-from . import views
-from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),  # 회원가입 url
     path('trade/', views.trade, name='trade'), # 중고거래화면 url
+    path('trade_post/<int:pk>/', views.trade_post, name='trade_post'), # 중고거래상세화면 url
     path('', views.main, name="main"), # 메인페이지 url
     path('login/', views.user_login, name='login'), # login url
     path('logout/', auth_views.LogoutView.as_view(next_page='main'), name='logout'), # logout url
