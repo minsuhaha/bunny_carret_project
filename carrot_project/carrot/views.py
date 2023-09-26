bf10a3552d8bb8c27c231b39888e67f91eb22459
rara0316 <duf1qudtls2@gmail.com>
2023-09-26 13:23:27 +0900

    feat : add <이수현/views.main>

--- carrot_project/carrot/views.py
+++ carrot_project/carrot/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def main(request):
+  top_views_products = Product.objects.filter(product_sold='N').order_by('-view_cnt')[:4]
+  return render(request, 'carret_app/main.html', {'products' : top_views_products})
--- carrot_project/carrot_project/settings.py
+++ carrot_project/carrot_project/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 PASSWORD_FILE = os.path.join(BASE_DIR, 'secret.json')
 secrets = json.load(open(PASSWORD_FILE))
+SECRET_KEY = secrets['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.humanize',
 
     # 앱명
     'carrot',
